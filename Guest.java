e8ee9e81540f1bf36c074e3e0b235ed771fa286c
sophiariley <70028371+sophiariley@users.noreply.github.com>
2022-03-15 13:50:25 -0400

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
 /**
+ * A class for guest users
  * 
+ * @author Sophia Riley
  */
-public class Guest implements User
-{
-    private ArrayList <Preferences> preferences;
+public class Guest implements User {
+    private ArrayList<Preferences> preferences;
     public String userID;
     public String firstName;
     public String lastName;
@@ -13,39 +14,46 @@ public class Guest implements User
     public String email;
     public String phoneNumber;
     public String address;
-    public ArrayList <Flight> flightBookings;
-    public ArrayList <Hotel> hotelBookings;
+    public ArrayList<Flight> flightBookings;
+    public ArrayList<Hotel> hotelBookings;
 
     /**
+     * Initializes the characteristics of a guest user
      * 
-     * @param firstName
-     * @param lastName
-     * @param passportID
-     * @param email
-     * @param phoneNumber
-     * @param address
+     * @param firstName   The user's first name
+     * @param lastName    The user's last name
+     * @param passportID  The user's passport ID
+     * @param email       The user's email
+     * @param phoneNumber The user's phone number
+     * @param address     The user's home addresss
      */
-    public Guest (String firstName, String lastName, String passportID, String email, String phoneNumber, String address)
-    {
+    public Guest(String firstName, String lastName, String passportID, String email, String phoneNumber,
+            String address) {
+        firstName = this.firstName;
+        lastName = this.lastName;
+        passportID = this.passportID;
+        email = this.email;
+        phoneNumber = this.phoneNumber;
+        address = this.address;
+    }
+
+    /**
+     * Gets the guest's name
+     * 
+     * @param firstName The user's first name
+     * @param lastName  The user's last name
+     */
+    public Guest(String firstName, String lastName) {
 
     }
 
     /**
+     * Gets the user's preferences
      * 
-     * @param firstName
-     * @param lastName
+     * @return preferences The user's preferences
      */
-    public Guest (String firstName, String lastName)
-    {
-
-    }
-
-    /**
-     * 
-     * @return
-     */
-    public ArrayList <Preferences> askPreferences()
-    {
-
+    private ArrayList<Preferences> askPreferences() {
+        preferences = new ArrayList<>();
+        return preferences;
     }
 }
