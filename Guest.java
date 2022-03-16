2add3f0e2c4b696be9357db6431c0e62e5859b43
Andrew Paulen <apaulen@MacBook-Pro-6.local>
2022-03-16 16:03:44 -0400

    gets&sets + error fixes in Guest

--- Guest.java
+++ Guest.java
@@ -17,6 +17,7 @@ public class Guest extends User {
     public ArrayList<Flight> flightBookings;
     public ArrayList<Hotel> hotelBookings;
 
+    // At this point they are a registered user, this method is not needed
     /**
      * Initializes the characteristics of a guest user
      * 
@@ -29,14 +30,15 @@ public class Guest extends User {
      */
     public Guest(String firstName, String lastName, String passportID, String email, String phoneNumber,
             String address) {
-        firstName = this.firstName;
-        lastName = this.lastName;
-        passportID = this.passportID;
-        email = this.email;
-        phoneNumber = this.phoneNumber;
-        address = this.address;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportID = passportID;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
 
+    // This is a constructor, this does not get the users name
     /**
      * Gets the guest's name
      * 
@@ -47,6 +49,7 @@ public class Guest extends User {
 
     }
 
+    // This should ask for preferences, not get the preferences
     /**
      * Gets the user's preferences
      * 
@@ -55,5 +58,105 @@ public class Guest extends User {
     private ArrayList<Preferences> askPreferences() {
         preferences = new ArrayList<>();
         return preferences;
+    }
+
+    public ArrayList <Preferences> getPreferences()
+    {
+        return preferences;
+    }
+
+    public void setPreferences (ArrayList <Preferences> preferences)
+    {
+        this.preferences = preferences;
+    }
+
+    public String getUserID()
+    {
+        return userID;
+    }
+
+    public void setUserID (String userID) 
+    {
+        this.userID = userID;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName (String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName (String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getPassportID()
+    {
+        return passportID;
+    }
+
+    public void setPassportID (String passportID)
+    {
+        this.passportID = passportID;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail (String email)
+    {
+        this.email = email;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber (String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress (String address)
+    {
+        this.address = address;
+    }
+
+    public ArrayList <Flight> getFlightBookings()
+    {
+        return flightBookings;
+    }
+
+    public void setFlightBookings (ArrayList <Flight> flightBookings)
+    {
+        this.flightBookings = flightBookings;
+    }
+
+    public ArrayList <Hotel> getHotelBookings()
+    {
+        return hotelBookings;
+    }
+
+    public void setHotelBookings (ArrayList <Hotel> hotelBookings)
+    {
+        this.hotelBookings = hotelBookings;
     }
 }
