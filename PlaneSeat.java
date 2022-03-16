66289b171f0551e1383fb7c6c99fa45b6579b721
Andrew Paulen <apaulen@MacBook-Pro-6.local>
2022-03-16 17:01:41 -0400

    added gets&sets for PlaneSeat clasS

--- PlaneSeat.java
+++ PlaneSeat.java
@@ -1,9 +1,40 @@
 /**
- * 
+ * An individual planeSeat
+ * @author Andrew Paulen
  */
 public class PlaneSeat 
 {
     private boolean hasPassenger;
     protected Orientation orientation;
     protected FlightClass flightClass;
+
+    public boolean getHasPassenger()
+    {
+        return hasPassenger;
+    }
+
+    public void setHasPassenger (boolean hasPassenger)
+    {
+        this.hasPassenger = hasPassenger;
+    }
+
+    public Orientation getOrientation()
+    {
+        return orientation;
+    }
+
+    public void setOrientation (Orientation orientation)
+    {
+        this.orientation = orientation;
+    }
+
+    public FlightClass getFlightClass()
+    {
+        return flightClass;
+    }
+
+    public void setFlightClass (FlightClass flightClass)
+    {
+        this.flightClass = flightClass;
+    }
 }
