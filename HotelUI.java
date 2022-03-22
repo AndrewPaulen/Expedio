97073267a03757d34460aa4c54734616f8bd2b10
Sophia Riley <riley.e.sophia@gmail.com>
2022-03-22 14:24:45 -0400

    Updated  FlightUI and HotelUI

--- HotelUI.java
+++ HotelUI.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class HotelUI {
-    
+
+    private Hotel flight;
+
+    public void bookHotel() {
+        System.out.println("*** Book Hotel ***\n\n");
+        hotels();
+    }
+
+    public void hotels() {
+        Scanner input = new Scanner(System.in);
+
+        availableHotels();
+    }
+
+    public void availableHotels() {
+        System.out.println("Fetching available hotels...\n\n" +
+                "Available hotels:\n");
+    }
 }
