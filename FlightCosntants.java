6e2622a2d40df3c7cf7b2846f9d92d9a44a4e483
ShashComandur <32526264+ShashankComandur@users.noreply.github.com>
2022-03-25 14:45:01 -0400

    Fixed all constant files

--- FlightCosntants.java
+++ FlightCosntants.java
@@ -3,7 +3,7 @@
  * an Enumeration of all JSON elements describing a flight. 
  */
 public class FlightCosntants {
-    protected static final String FLIGHTS_FILE_NAME = "Expedio\\flights.json";
+    protected static final String FLIGHTS_FILE_NAME = "flights.json";
     protected static final String FLIGHT_ID = "flight-id";
     protected static final String DEPARTURE_LOC = "departureLocation";
     protected static final String ARRIVAL_LOC = "arrivalLocation";
--- HotelConstants.java
+++ HotelConstants.java
@@ -1,10 +1,9 @@
-
 import java.util.UUID;
 /**
  * an Enumeration of all json elements that comprise Hotel
  */
 public class HotelConstants {
-    protected static final String HOTEL_FILE_NAME = "Expedio/hotels.json";
+    protected static final String HOTEL_FILE_NAME = "hotels.json";
     protected static final String HOTEL_NAME = "name";
     protected static final String HOTEL_ID = "id";
     protected static final String HOTEL_LOCATION = "location";
